ace3b079c637929283fa1b0ea91c11411e131afd
MunozMaciasS <112488064+MunozMaciasS@users.noreply.github.com>
2022-10-07 15:45:18 -0400

    Update TP1_Samuel_Munoz.py

--- TP1_Samuel_Munoz.py
+++ TP1_Samuel_Munoz.py
@@ -2,10 +2,10 @@
 #Cree en septembre 2022
 #But:compter les nombres des mots dans un pharase syntaxique ou pas syntaxique
 
-def word_count():
+phrase = input("ecrivez une phrase quelconque")
+def word_count(phrase):
 
-    phrase = input("ecrivez une phrase quelconque")
     nbr_des_mots = str(len(phrase.split(" "))) #on se pare les mots en diffrents sous chianes et on nomme les nbr des chaines
     return nbr_des_mots
 
-print("il y a",word_count(),"mots") #on print le resultat
+print("il y a",word_count(phrase),"mots") #on print le resultat
